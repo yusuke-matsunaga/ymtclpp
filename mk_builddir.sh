d4ca2d141486e4c1bf82952ce5e20be8eaea21ce
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2015-07-25 06:21:31 +0900

    ﻿mk_builddir.sh, etc/do_cmake.sh.in の細かな修正
    * 主に説明用のコメントの追加

--- mk_builddir.sh
+++ mk_builddir.sh
@@ -1,24 +1,32 @@
 #! /bin/sh
-
+#
 # mk_builddir.sh
 #
 # コンパイル用のディレクトリを作るシェルスクリプト
 #
-# Copyright (C) 2015 Yusuke Matsunaga
+# Copyright (C) 2015 Yusuke Matsunaga (松永 裕介)
 # All rights reserved
 
+# コマンドの定義
+# 環境変数 SED をセットしておけばその値が使われる．
+# そうでなければデフォルトの sed が使われる．
+echo ${SED:="sed"} > /dev/null
 
 # このファイルのあるディレクトリを srcdir にセットする．
 basedir=`dirname $0`
 srcdir=`cd $basedir; pwd`
 
+# オプション解析用のループ
 while [ $# -ge 4 ] ; do
-    if [ $1 = "--ymtools_dir" ]; then
-	ymtoolsdir=$2
-	shift; shift
-    fi
+    case $1 in
+	"--ymtools_dir")
+	    ymtoolsdir=$2
+	    shift; shift;;
+	*) ;;
+    esac
 done
 
+# 引数の数が異なっていたら usage を表示して終わる．
 if [ $# -ne 2 ]; then
     echo "USAGE mk_builddir.sh [--ymtools_dir <ymtools-dir>] <compiledir> <installdir>"
     exit 1
@@ -30,6 +38,7 @@ builddir=$1
 # インストール先のディレクトリ名
 installdir=$2
 
+# ディレクトリ名を表示して確認を求める．
 echo "****"
 echo "source  directory: $srcdir"
 echo "build   directory: $builddir"
@@ -55,16 +64,16 @@ done
 # ビルドディレクトリはなければ作る．
 test -d $builddir || mkdir -p $builddir
 
-# do_cmake ファイルを作る．
+# do_cmake.sh スクリプトを作る．
 do_cmake="do_cmake.sh"
-sed "-e s!___SRC_DIR___!$srcdir!" \
-    "-e s!___INSTALL_DIR___!$installdir!" \
-    "-e s!___YMTOOLS_DIR___!$ymtoolsdir!" \
-    ${srcdir}/etc/${do_cmake}.in > ${builddir}/${do_cmake}
-chmod +x $builddir/${do_cmake}
+$SED -e s!___SRC_DIR___!$srcdir! \
+     -e s!___INSTALL_DIR___!$installdir! \
+     -e s!___YMTOOLS_DIR___!$ymtoolsdir! \
+     $srcdir/etc/${do_cmake}.in > $builddir/$do_cmake
+chmod +x $builddir/$do_cmake
 
 # do_cmake.sh を実行する．
 echo "running cmake"
-(cd $builddir && ./${do_cmake})
+(cd $builddir && ./$do_cmake)
 
 echo "done"
