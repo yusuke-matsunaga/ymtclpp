ba7624eb38eb26b7b8e0f9ca809fee44c7ed3cc3
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2015-08-05 11:38:28 +0900

    ﻿cmake/ym_add_library.cmake 修正
    - コメントの追加とコードの整理

--- cmake/ym_add_library.cmake
+++ cmake/ym_add_library.cmake
@@ -7,12 +7,25 @@ function (ym_add_library)
     endif ()
     list (GET ARGV ${pos} argv)
     if ( ${pos} EQUAL 0 )
-      set (_target_name ${argv})
+      # ターゲット名の設定
+      # - 最適化モード
+      # - プロファイルモード (_p)
+      # - デバッグモード (_d)
+      # の3つがある．
+      set (_target_name "${argv}")
+      set (_target_name_p "${argv}_p")
+      set (_target_name_d "${argv}_d")
     elseif ( ${pos} EQUAL 1 )
       if ( ${argv} STREQUAL "SHARED" )
 	set (_option "SHARED")
+	# debug モードでは常にスタティック
+	set (_dbg_option "STATIC")
       elseif ( ${argv} STREQUAL "STATIC" )
 	set (_option "STATIC")
+	set (_dbg_option "STATIC")
+      elseif ( ${argv} STREQUAL "OBJECT" )
+	set (_option "OBJECT")
+	set (_dbg_option "OBJECT")
       else ()
 	list (APPEND _sources ${argv})
       endif ()
@@ -36,7 +49,6 @@ function (ym_add_library)
     )
 
   # release with debug info モードのターゲット
-  set (_target_name_p ${_target_name}_p)
   add_library (${_target_name_p}
     ${_option}
     ${_sources}
@@ -51,10 +63,9 @@ function (ym_add_library)
     )
 
   # debug モードのターゲット
-  set (_target_name_d ${_target_name}_d)
+  # 常に static にする．
   add_library (${_target_name_d}
-    # 常に static にする．
-    STATIC
+    ${_dbg_option}
     ${_sources}
     )
 
